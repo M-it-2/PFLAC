2c5f4331aaed1c404edab420fddc7329e024a6b0
fxhxyz4 <fxhxyz8@gmail.com>
2026-01-25 18:17:43 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -37,7 +37,9 @@ then
 
     elif [ -f /etc/arch-release ]; then
         # Arch Linux
-        sudo pacman -Sy docker --noconfirm --needed
+       sudo pacman -S --needed containerd runc docker
+       sudo systemctl enable --now containerd
+       sudo systemctl enable --now docker.socket
 
     elif [ -f /etc/fedora-release ]; then
         # Fedora
