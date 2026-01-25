6ae5a0bf6578eb2040e32564e4d5ba0e2af0edbd
fxhxyz4 <fxhxyz8@gmail.com>
2026-01-25 17:58:20 +0200

    Fix schema file name in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -172,7 +172,7 @@ fi
 echo "[INFO] Run MySQL..."
 docker rm -f mysql_local >/dev/null 2>&1 || true
 
-SCHEMA_FILE="$API_DIR/db/schema.sql"
+SCHEMA_FILE="$API_DIR/db/scheme.sql"
 
 if [ ! -f "$SCHEMA_FILE" ]; then
     echo "[ERROR] SQL-file $SCHEMA_FILE not found!"
